1fb0fbfceef2ed4f9a816bcab3cdde5bbfca1f25
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2025-11-17 10:02:47 +0700

    HomingBullet.lua --> v1.0.1
    Added a check for if the target is nil

--- HomingWeapon/HomingBullet.lua
+++ HomingWeapon/HomingBullet.lua
@@ -1,14 +1,13 @@
-behaviour("HomingBullet") --v1.0.0
+behaviour("HomingBullet") --1.0.1
 
 --function HomingBullet:Start()
 --    self.projectile = self.gameObject.GetComponent(Projectile)
 --end
 
 function HomingBullet:Update()
-    if (self.projectile == nil) then
+    if (self.projectile == nil) or (self.target == nil) then
         return
     end
 
     self.projectile.velocity = Vector3.RotateTowards(self.projectile.velocity, self.target.centerPosition - self.transform.position, self.aimSpeed * Time.deltaTime, 0)
-
 end
