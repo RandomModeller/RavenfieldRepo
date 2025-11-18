a4f94bebd2afbcad463672dfdcb695eb904e7b3a
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2025-11-18 22:09:09 +0700

    SetProjectileLineRendererOnEnd.lua v1.1.0
    Fixed bug that makes the start position not constant

--- SetProjectileLineRendererOnEnd.lua
+++ SetProjectileLineRendererOnEnd.lua
@@ -1,4 +1,4 @@
-behaviour("SetProjectileLineRendererOnEnd")
+behaviour("SetProjectileLineRendererOnEnd") --v1.1.0
 
 function SetProjectileLineRendererOnEnd:Start()
     self.lineRenderer = self.targets.lineRenderer.GetComponent(LineRenderer)
@@ -6,11 +6,11 @@ function SetProjectileLineRendererOnEnd:Start()
     self.transform = self.gameObject.transform
 
     self.last = self.transform.position
+    self.lineRenderer.SetPosition(0, self.transform.position)
 end
 
 function SetProjectileLineRendererOnEnd:Update()
     if self.last ~= self.transform.position then
-        self.lineRenderer.SetPosition(0, self.last)
         self.lineRenderer.SetPosition(1, self.transform.position)
         self.last = self.transform.position
     end
