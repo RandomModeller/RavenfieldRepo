3c87ab785ae2ee338399f92cb99c10b302ec5438
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-01-17 01:43:11 +0700

    Update AddRigidbodyExplosion.lua --> v1.1.0
    Ability to set the chance of turret toss
    Ability to set forceMin/forceMax

--- AddRigidbodyExplosion.lua
+++ AddRigidbodyExplosion.lua
@@ -1,9 +1,21 @@
-behaviour("AddRigidbodyExplosion") --v1.0.0
+behaviour("AddRigidbodyExplosion") --v1.1.0
 
 function AddRigidbodyExplosion:Start()
     self.rigidbody = self.gameObject.GetComponent(Rigidbody)
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
     self.centre = self.targets.centre.transform.position
 
-    self.rigidbody.AddExplosionForce(self.dataContainer.GetFloat("force"), self.centre, 25)
+    self.chance = 1
+
+    if self.dataContainer.HasFloat("chance") then
+        self.chance = self.dataContainer.GetFloat("chance")
+    end
+
+    if Random.Range(0, 1) <= self.chance then
+        if self.dataContainer.HasFloat("force") then
+            self.rigidbody.AddExplosionForce(self.dataContainer.GetFloat("force"), self.centre, 25)
+        else
+            self.rigidbody.AddExplosionForce(Mathf.Lerp(self.dataContainer.GetFloat("forceMin"), self.dataContainer.GetFloat("forceMax"), Random.Range(0, 1)), self.centre, 25)
+        end
+    end
 end
