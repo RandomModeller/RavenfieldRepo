451035eb694cf7d578c4e50fdeea899ca43b4ef4
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-01-31 22:26:26 +0700

    Update LogisticSupply.lua v1.1.0
    Add weapon and dropOnPlayerExit

--- LogisticSupply.lua
+++ LogisticSupply.lua
@@ -1,11 +1,18 @@
-behaviour("LogisticSupply") --v1.0.0
+behaviour("LogisticSupply") --v1.1.0
 
 function LogisticSupply:Start()
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
 
     self.vehicleObject = self.targets.vehicleObject.GetComponent(Vehicle)
+    if self.targets.weapon ~= nil then
+        self.weapon = self.targets.weapon.GetComponent(Weapon)
+    end
     self.reloadTime = self.dataContainer.GetFloat("reloadTime")
     self.spawnOffset = self.dataContainer.GetVector("spawnOffset")
+    self.dropOnPlayerExit = true
+    if self.dataContainer.HasBool("dropOnPlayerExit")
+        self.dropOnPlayerExit = self.dataContainer.GetBool("dropOnPlayerExit")
+end
     self.logisticBox = self.targets.logisticBox
 
     self.nextDeployTime = 0
@@ -26,7 +33,17 @@ function LogisticSupply:Update()
             end
         end
 
-        if ((driverIsIn and self.lastDriverIsIn) and (not hasPassengers and self.lastHasPassengers)) or (not playerIsInside and self.lastPlayerIsInside) then
+        local flag1 = (driverIsIn and self.lastDriverIsIn) and (not hasPassengers and self.lastHasPassengers)
+
+        local flag2 = not playerIsInside and self.lastPlayerIsInside and self.dropOnPlayerExit
+
+        local flag3 = false
+        if self.weapon ~= nil then
+            local flag3 = self.weapon.isHoldingFire and self.weapon.canFire
+            
+        end
+
+        if flag1 or flag2 or flag3 then
             GameObject.Instantiate(self.logisticBox, self.vehicleObject.transform.position + self.spawnOffset.x * self.vehicleObject.transform.right + self.spawnOffset.y * self.vehicleObject.transform.up + self.spawnOffset.z * self.vehicleObject.transform.forward, self.vehicleObject.transform.rotation)
 
             self.nextDeployTime = Time.time + self.reloadTime
