5c1ba6895f3d20b93faeee243e30c3beba5860ce
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-02-03 18:50:06 +0700

    Update TVPCamoChanger.lua --> v1.1.0
    Added ability to toggle objects

--- TVPCamoChanger.lua
+++ TVPCamoChanger.lua
@@ -1,4 +1,4 @@
-behaviour("TVPCamoChanger") --v1.0.0
+behaviour("TVPCamoChanger") --v1.1.0
 
 function TVPCamoChanger:Start()
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
@@ -13,5 +13,13 @@ function TVPCamoChanger:OnDropdownChange(newValue)
     local newColor = self.dataContainer.GetColor("color" .. (newValue + 1))
     for i, material in pairs(self.materials) do
         material.color = newColor
+
+        for j=1, #self.materials do
+            for k, object in self.dataContainer.GetGameObjectArray("object" .. tostring(k)) do
+                if object ~= nil then
+                    object.SetActive(j == newValue + 1)
+                end
+            end
+        end
     end
 end
