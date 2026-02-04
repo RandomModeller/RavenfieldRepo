4d44fe294301b707e144d354cf9a9529df6e2a47
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-02-04 17:43:37 +0700

    Update TankSuspension.lua --> v1.1.0
    Add support for no vertical

--- TankSuspension.lua
+++ TankSuspension.lua
@@ -1,15 +1,17 @@
-behaviour("TankSuspension") --v1.0.0
+behaviour("TankSuspension") --v1.1.0
 
 function TankSuspension:Start()
     self.pointA = self.targets.pointA.transform
     self.pointB = self.targets.pointB.transform
-    self.vertical = self.targets.vertical.transform
+    if self.targets.vertical ~= nil then
+        self.vertical = self.targets.vertical.transform
+        self.verticalDefault = self.vertical.localPosition
+    end
     self.rotate = self.targets.rotate.transform
     self.vehicleObject = self.targets.vehicleObject.transform
 
     self.pointADefault = self.pointA.localPosition.y
     self.pointBDefault = self.pointB.localPosition.y
-    self.verticalDefault = self.vertical.localPosition
     self.rotateDefault = self.rotate.localPosition
 end
 
@@ -17,7 +19,11 @@ function TankSuspension:Update()
     local aDeviation = self.pointA.localPosition.y - self.pointADefault
     local bDeviation = self.pointB.localPosition.y - self.pointBDefault
 
-    self.vertical.localPosition = self.verticalDefault + Vector3.forward * ((aDeviation + bDeviation) / 2)
-    self.vertical.rotation = Quaternion.LookRotation(self.pointA.position - self.pointB.position, self.vehicleObject.up)
-    self.rotate.rotation = Quaternion.LookRotation(self.vertical.position - self.rotate.position, self.vehicleObject.up)
+    if self.vertical ~= nil then
+        self.vertical.localPosition = self.verticalDefault + Vector3.forward * ((aDeviation + bDeviation) / 2)
+        self.vertical.rotation = Quaternion.LookRotation(self.pointA.position - self.pointB.position, self.vehicleObject.up)
+        self.rotate.rotation = Quaternion.LookRotation(self.vertical.position - self.rotate.position, self.vehicleObject.up)
+    else
+        self.rotate.rotation = Quaternion.LookRotation(self.pointB.position - self.rotate.position, self.vehicleObject.up)
+    end
 end
