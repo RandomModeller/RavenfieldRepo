f8baa7e772ec1f67375f1c0e37da77274fb01bc6
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-02-10 22:56:06 +0700

    Update DynamicHydropneumatic.lua --> v1.0.1
    Fix bug when vehicle is backwards

--- DynamicHydropneumatic.lua
+++ DynamicHydropneumatic.lua
@@ -1,4 +1,4 @@
-behaviour("DynamicHydropneumatic") --v1.0.0
+behaviour("DynamicHydropneumatic") --v1.0.1
 
 function DynamicHydropneumatic:Start()
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
@@ -15,6 +15,12 @@ function DynamicHydropneumatic:Start()
     self.pitchMinNormal = self.dataContainer.GetFloat("pitchMinNormal")
     self.pitchMinExtra = self.dataContainer.GetFloat("pitchMinExtra")
     self.speed = self.dataContainer.GetFloat("speed")
+
+    if self.targets.audio ~= nil then
+        self.audio = self.targets.audio.GetComponent(AudioSource)
+        self.last = self.originalPitch.localRotation
+    end
+
 end
 
 function DynamicHydropneumatic:Update()
@@ -23,16 +29,18 @@ function DynamicHydropneumatic:Update()
 
     local targetRotation = Quaternion.identity
 
+    local delta = 0
+
     if pitch > self.pitchMaxNormal or pitch < self.pitchMinNormal then
-        local delta = 0
         
         if pitch > self.pitchMaxNormal then
             delta = self.pitchMaxNormal - pitch
         elseif pitch < self.pitchMinNormal then
             delta = self.pitchMinNormal - pitch
         end
+        -- print(delta)
 
-        targetRotation = Quaternion.AngleAxis(-Mathf.Round(delta * 2) / 2, self.originalBearing.right) -- round it to nearest multiple of 0.5 prevent jitter
+        targetRotation = Quaternion.AngleAxis(-Mathf.Round(delta * 1.5) / 1.5, Quaternion.AngleAxis(-self.hull.parent.localEulerAngles.y, Vector3.up) * self.originalBearing.right) -- round it to nearest multiple of 0.5 prevent jitter
     end
 
     self.hull.localRotation = Quaternion.RotateTowards(self.hull.localRotation, targetRotation, self.speed * Time.deltaTime)
@@ -41,4 +49,18 @@ function DynamicHydropneumatic:Update()
     rotation.x = Mathf.Clamp(pitch, self.pitchMinNormal, self.pitchMaxNormal)
 
     self.originalPitch.localEulerAngles = rotation
+
+    -- if self.audio ~= nil then
+        -- local playAudio = Quaternion.Angle(self.last, self.hull.localRotation) > 0.00001
+
+        -- if self.audio.isPlaying ~= playAudio then
+        --     if playAudio then
+        --         self.audio:Play()
+        --     else
+        --         self.audio:Stop()
+        --     end
+        -- end
+
+    -- self.last = self.hull.localEulerAngles.x
+    -- end
 end
