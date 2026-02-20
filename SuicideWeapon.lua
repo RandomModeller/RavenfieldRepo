b808bb3b4c7716a274358f6609b3ab685c9d9ff7
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-02-20 15:44:11 +0700

    Upload SuicideWeapon.lua

---
+++ SuicideWeapon.lua
@@ -0,0 +1,15 @@
+behaviour("SuicideWeapon")
+
+function SuicideWeapon:Start()
+    if self.targets.weaponObject ~- nil then
+        self.weaponObject = self.targets.weaponObject.GetComponent(Weapon)
+    else
+        self.weaponObject = self.gameObject.GetComponent(Weapon)
+    end
+
+    self.weaponObject.onSpawnProjectiles.AddListener(self, "OnFire")
+end
+
+function SuicideWeapon:OnFire()
+    self.weaponObject.user:Kill(self.weaponObject.user)
+end
