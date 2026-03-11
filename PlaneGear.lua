88e4dfa6747be9ad71f41af82ef81dca0cc4d027
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-03-11 09:42:06 +0700

    Add PlaneGear v1.0.0

---
+++ PlaneGear.lua
@@ -0,0 +1,58 @@
+behaviour("PlaneGear") --v1.0.0
+
+function PlaneGear:Start()
+    self.animator = self.targets.animator.GetComponent(Animator)
+    self.vehicle = self.targets.vehicleObject.GetComponent(Vehicle)
+    self.rigidbody = self.vehicle.rigidbody
+    self.heightChecker = self.targets.heightChecker.GetComponent(ScriptedBehaviour).self
+
+    self.name = self.animator.StringToHash(self.gameObject.GetComponent(DataContainer).GetString("name"))
+
+    self.gearDown = true
+
+    self.drag = 0.045
+
+    self.loadedKeybind = DebilFalconConfig_ConfigLoaded
+    if self.loadedKeybind then
+        self:LoadKeybind()
+    end
+end
+
+function PlaneGear:Update()
+    if not self.loadedKeybind then
+        self:LoadKeybind()
+
+        self.loadedKeybind = true
+    end
+
+    if self.vehicle.playerIsInside then
+        if Input.GetKeyDown(self.key) then
+            self.gearDown = not self.gearDown
+
+            self.animator.SetBool(self.name, self.gearDown)
+
+            if self.gearDown then
+                self.rigidbody.drag = self.rigidbody.drag + self.drag
+            else
+                self.rigidbody.drag = self.rigidbody.drag - self.drag
+            end
+        end
+    else
+        self.animator.SetBool(self.name, self.heightChecker.height <= 20)
+    end
+end
+
+function PlaneGear:LoadKeybind()
+    local script = GameObject.Find("F-16 Config(Clone)")
+
+    if script then
+        script = script.GetComponent(ScriptedBehaviour).self
+       
+        if script then
+            self.key = script.DebilFalconConfig_GearToggle
+        end
+    end
+    if self.key == nil then
+        self.key = KeyCode.Z
+    end
+end
