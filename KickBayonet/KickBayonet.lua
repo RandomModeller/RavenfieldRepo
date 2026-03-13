c01c2af2301aac24f445d9c14f5ba56c30ca8edb
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-03-13 11:26:12 +0700

    Update and move KickBayonet.lua --> v2.0.0
    Rework to the system and some bug fixes

---
+++ KickBayonet/KickBayonet.lua
@@ -0,0 +1,133 @@
+behaviour("KickBayonet") --v2.0.0
+
+function KickBayonet:Start()
+    if self.weapon == nil then
+        self.weapon = self.targets.weapon.GetComponent(Weapon)
+    end
+
+    if self.weapon.user ~= Player.actor then
+        return
+    end
+
+    self.animator = self.targets.weapon.GetComponent(Animator)
+    self.audioSource = self.targets.audioSource.GetComponent(AudioSource)
+    self.dataContainer = self.gameObject.GetComponent(DataContainer)
+
+    self.kickParameter = self.animator.StringToHash("kick")
+    self.allowWhenProne = self.dataContainer.GetBool("allowWhenProne")
+    self.allowWhenAim = self.dataContainer.GetBool("allowWhenAim")
+    self.kickRadius = self.dataContainer.GetFloat("kickRadius")
+    self.kickRange = self.dataContainer.GetFloat("kickRange")
+    self.damage = self.dataContainer.GetFloat("damage")
+    self.balanceDamage = self.dataContainer.GetFloat("balanceDamage")
+    self.force = self.dataContainer.GetFloat("force")
+    self.cooldown = self.dataContainer.GetFloat("cooldown")
+    self.delay = self.dataContainer.GetFloat("delay")
+    self.swing = self.dataContainer.GetAudioClip("swing")
+    self.hit = self.dataContainer.GetAudioClip("hit")
+
+    self.requiresEquip = false
+    if self.isEquipped == nil then
+        self.isEquipped = true
+    end
+
+    if self.dataContainer.HasBool("requiresEquip") then
+        self.requiresEquip = self.dataContainer.GetBool("requiresEquip")
+
+        if self.requiresEquip then
+            self.equipKeybind = self.dataContainer.GetString("equipKeybind")
+            self.animatorParameter = self.animator.StringToHash(self.dataContainer.GetString("isEquippedParameterName"))
+
+            self.isEquipped = self.dataContainer.GetBool("isEquipped")
+            self.animator.SetBool(self.animatorParameter, self.isEquipped)
+        end
+    end
+
+    self.nextKick = Time.time
+    self.kickTime = 0
+end
+
+function KickBayonet:Update()
+    if self.weapon.user ~= Player.actor then
+        return
+    end
+
+    if self.requiresEquip then
+        if Input.GetKeyDown(self.equipKeybind) then
+            self.isEquipped = not self.isEquipped
+            self.animator.SetBool(self.animatorParameter, self.isEquipped)
+
+            Player.allowKick = not self.isEquipped
+        end
+    end
+
+    if self.isEquipped and Input.GetKeyBindButtonDown(KeyBinds.Kick) and not self.weapon.isReloading and self.weapon.isUnholstered and Time.time >= self.nextKick and (self.allowWhenAim or not self.weapon.isAiming) and (self.allowWhenProne or not (Player.actor.stance == Stance.Prone)) then
+        self.animator.SetTrigger(self.kickParameter)
+
+        if self.swing ~= nil then
+            self.audioSource.PlayOneShot(self.swing)
+        end
+
+        self.kickTime = Time.time + self.delay
+        self.nextKick = Time.time + self.cooldown
+    end
+
+    if self.kickTime ~= 0 then
+        if Time.time >= self.kickTime then
+            local ray = Ray(PlayerCamera.fpCamera.transform.position + PlayerCamera.fpCamera.transform.forward * 0.55, PlayerCamera.fpCamera.transform.forward)
+            local raycast = Physics.Spherecast(ray, self.kickRadius, self.kickRange, RaycastTarget.ProjectileHit)
+            if raycast ~= nil then
+                if raycast.transform.gameObject.layer == 8 then
+                    local actorComponent = raycast.transform.gameObject.GetComponentInParent(Actor)
+
+                    actorComponent.Damage(self.weapon.user, self.damage, self.balanceDamage, false, false, raycast.point, PlayerCamera.fpCamera.transform.forward, PlayerCamera.fpCamera.transform.forward * self.force)
+                    if self.hit ~= nil then
+                        self.audioSource.PlayOneShot(self.hit)
+                    end
+                end
+            end
+
+            self.kickTime = 0
+        end
+    end
+end
+
+function KickBayonet:OnEnable()
+    if self.weapon == nil then
+        self.weapon = self.targets.weapon.GetComponent(Weapon)
+    end
+
+    if self.weapon.user ~= Player.actor then
+        return
+    end
+
+    if self.isEquipped == nil then
+        self.isEquipped = true
+    end
+
+    Player.allowKick = not self.isEquipped
+end
+
+function KickBayonet:OnDisable()
+    if self.weapon == nil then
+        self.weapon = self.targets.weapon.GetComponent(Weapon)
+    end
+
+    if self.weapon.user ~= Player.actor then
+        return
+    end
+
+    Player.allowKick = true
+end
+
+function KickBayonet:OnDestroy()
+    if self.weapon == nil then
+        self.weapon = self.targets.weapon.GetComponent(Weapon)
+    end
+
+    if self.weapon.user ~= Player.actor then
+        return
+    end
+
+    Player.allowKick = true
+end
