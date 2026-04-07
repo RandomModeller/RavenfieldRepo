ea4afd7be9645afc97e3f4c407ed9585ec77c7ef
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-04-07 12:22:52 +0700

    Update CustomAmmoBelt.lua --> v1.1.0

--- CustomAmmoBelt.lua
+++ CustomAmmoBelt.lua
@@ -1,4 +1,4 @@
-behaviour("CustomAmmoBelt") --v1.0.0
+behaviour("CustomAmmoBelt") --v1.1.0
 
 function CustomAmmoBelt:Start()
     self.weapon = self.gameObject.GetComponent(Weapon)
@@ -8,10 +8,19 @@ function CustomAmmoBelt:Start()
 
     self.belt = self.dataContainer.GetGameObjectArray("projectile")
 
+    self.fireRandom = false
+    if self.dataContainer.HasBool("fireRandom") then
+        self.fireRandom = self.dataContainer.GetBool("fireRandom")
+    end
+
     self.counter = 0
 end
 
 function CustomAmmoBelt:onFire()
+    if self.fireRandom then
+        self.weapon.SetProjectilePrefab(self.belt[Mathf.Round(Random.Range(1, #self.belt))])
+    end
+
     self.counter = (self.counter + 1) % #self.belt
 
     self.weapon.SetProjectilePrefab(self.belt[self.counter + 1])
