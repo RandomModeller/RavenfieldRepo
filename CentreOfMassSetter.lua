f5c8c31432ee6246dd4dfab1a0c685a523a8a22f
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-04-13 12:54:15 +0700

    Update CentreOfMassSetter.lua --> v1.0.2d

--- CentreOfMassSetter.lua
+++ CentreOfMassSetter.lua
@@ -1,6 +1,15 @@
-behaviour("CentreOfMassSetter") --v1.0.1
+behaviour("CentreOfMassSetter") --v1.0.2d
 
-function CentreOfMassSetter:Start()
+function CentreOfMassSetter:Update()
+    if self.finished then
+        return
+    end
+
+    self:BakeRigidbody()
+    self.finished = true
+end
+
+function CentreOfMassSetter:BakeRigidbody()
     if self.targets.rigidbody ~= nil then
         self.rigidbody = self.targets.rigidbody.GetComponent(Rigidbody)
     else
@@ -10,4 +19,9 @@ function CentreOfMassSetter:Start()
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
 
     self.rigidbody.centerOfMass = self.dataContainer.GetVector("centreOfMass")
+
+    if self.targets.debugCube then
+        self.targets.debugCube.transform.localPosition = self.rigidbody.centerOfMass
+        print("moved the cube to " .. tostring(self.rigidbody.centerOfMass))
+    end
 end
