2ae3b89873f0b287154e245c46d90d13192e71cc
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-04-14 15:36:49 +0700

    Add SharedAmmoPoolThree.lua v1.0.0

---
+++ SharedAmmoPoolThree.lua
@@ -0,0 +1,38 @@
+behaviour("SharedAmmoPoolThree") --v1.0.0
+
+function SharedAmmoPoolThree:Start()
+    self.mainWep = self.targets.main.GetComponent(Weapon)
+    self.secondaryWep = self.targets.secondary.GetComponent(Weapon)
+    self.tertiaryWep = self.targets.tertiary.GetComponent(Weapon)
+end
+
+function SharedAmmoPoolThree:Update()
+    local activeWeapon
+
+    if self.mainWep.activeSubWeapon == self.secondaryWep then
+        activeWeapon = self.secondaryWep
+    elseif self.mainWep.activeSubWeapon == self.tertiaryWep then
+        activeWeapon = self.tertiaryWep
+    else
+        activeWeapon = self.mainWep
+    end
+
+    if Input.GetKeyBindButtonDown(KeyBinds.FireMode) then
+        if activeWeapon.isReloading then
+            return
+        end
+
+        if activeWeapon == self.mainWep then            
+            self.mainWep.ammo = self.tertiaryWep.ammo
+            self.mainWep.spareAmmo = self.tertiaryWep.spareAmmo
+        end
+        if activeWeapon == self.secondaryWep then
+            self.secondaryWep.ammo = self.mainWep.ammo
+            self.secondaryWep.spareAmmo = self.mainWep.spareAmmo
+        end
+        if activeWeapon == self.tertiaryWep then
+            self.secondaryWep.ammo = self.secondaryWep.ammo
+            self.secondaryWep.spareAmmo = self.secondaryWep.spareAmmo
+        end
+    end
+end
