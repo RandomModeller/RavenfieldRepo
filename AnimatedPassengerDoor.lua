a426c2c69fba7d6c45394f6b7fdb44a7c4bcc410
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-05-06 19:26:56 +0700

    Update AnimatedPassengerDoor.lua --> v1.1.4
    Added more customization

--- AnimatedPassengerDoor.lua
+++ AnimatedPassengerDoor.lua
@@ -1,4 +1,4 @@
-behaviour("AnimatedPassengerDoor") --v1.1.3
+behaviour("AnimatedPassengerDoor") --v1.1.4
 
 function AnimatedPassengerDoor:Start()
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
@@ -12,14 +12,33 @@ function AnimatedPassengerDoor:Start()
         self.keybind = self.dataContainer.GetString("keybind")
     end
 
+    self.lowHeight = 8
+    if self.dataContainer.HasFloat("lowHeight") then
+        self.lowHeight = self.dataContainer.GetFloat("lowHeight")
+    end
+    self.midHeight = 18
+    if self.dataContainer.HasFloat("midHeight") then
+        self.midHeight = self.dataContainer.GetFloat("midHeight")
+    end
+    self.velocitySqr = 200
+    if self.dataContainer.HasFloat("velocitySqr") then
+        self.velocitySqr = self.dataContainer.GetFloat("velocitySqr")
+    end
+    self.horizontalRangeSqr = 25000
+    if self.dataContainer.HasFloat("horizontalRangeSqr") then
+        self.horizontalRangeSqr = self.dataContainer.GetFloat("horizontalRangeSqr")
+    end
+
     self.lastAutoValue = 2
 end
 
 function AnimatedPassengerDoor:Update()
     local isFull = self.vehicle.GetEmptySeat(false) == nil
-    local isLow = self.heightChecker.height <= 8
-    local isSlow = self.vehicle.rigidbody.velocity.sqrMagnitude <= 200
+    local isLow = self.heightChecker.height <= self.lowHeight
+    local isMedium = self.heightChecker.height <= self.midHeight
+    local isSlow = self.vehicle.rigidbody.velocity.sqrMagnitude <= self.velocitySqr
     local isCloseToLZ = false
+    local isCloseToCapturePoint = false
     local isPickingUp = false
     
     if self.vehicle.driver ~= nil then
@@ -27,14 +46,25 @@ function AnimatedPassengerDoor:Update()
             if self.vehicle.driver.squad.claimedLandingZone ~= nil then
                 isCloseToLZ = self.vehicle.driver.squad.claimedLandingZone.position - self.vehicle.transform.position
                 isCloseToLZ.y = 0
-                isCloseToLZ = isCloseToLZ.sqrMagnitude <= 15000
+                isCloseToLZ = isCloseToLZ.sqrMagnitude <= self.horizontalRangeSqr
             end
+
+            if self.vehicle.driver.squad.order ~= nil then
+                if self.vehicle.driver.squad.order.type == OrderType.Attack then
+                    if self.vehicle.driver.squad.order.targetPoint ~= nil then
+                        isCloseToCapturePoint = self.vehicle.driver.squad.order.targetPoint.transform.position - self.vehicle.transform.position
+                        isCloseToCapturePoint.y = 0
+                        isCloseToCapturePoint = isCloseToCapturePoint.sqrMagnitude <= self.horizontalRangeSqr
+                    end
+                end
+            end
+
 
             isPickingUp = self.vehicle.driver.squad.hasLanded or self.vehicle.driver.squad.isPerformingLanding
         end
     end
     
-    local doorOpen = (isLow and not isFull and isSlow) or (isCloseToLZ) or (isPickingUp)
+    local doorOpen = (isLow and not isFull and isSlow) or (isCloseToLZ or isCloseToCapturePoint) or (isPickingUp and isMedium)
 
     if self.keybind then
         if Input.GetKeyDown(self.keybind) and self.vehicle.playerIsInside then
