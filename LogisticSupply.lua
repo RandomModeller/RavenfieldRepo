32d32257fb01c9fc732433cc94d222a613030c6e
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-05-06 10:27:19 +0700

    Update LogisticSupply.lua --> v1.2.0
    More integration with weapon

--- LogisticSupply.lua
+++ LogisticSupply.lua
@@ -1,19 +1,21 @@
-behaviour("LogisticSupply") --v1.1.1
+behaviour("LogisticSupply") --v1.2.0
 
 function LogisticSupply:Start()
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
 
     self.vehicleObject = self.targets.vehicleObject.GetComponent(Vehicle)
-    if self.targets.weapon ~= nil then
+    if self.targets.weapon == nil then
+        self.reloadTime = self.dataContainer.GetFloat("reloadTime")
+        self.spawnOffset = self.dataContainer.GetVector("spawnOffset")
+        self.logisticBox = self.targets.logisticBox
+    else
         self.weapon = self.targets.weapon.GetComponent(Weapon)
     end
-    self.reloadTime = self.dataContainer.GetFloat("reloadTime")
-    self.spawnOffset = self.dataContainer.GetVector("spawnOffset")
+
     self.dropOnPlayerExit = true
     if self.dataContainer.HasBool("dropOnPlayerExit") then
         self.dropOnPlayerExit = self.dataContainer.GetBool("dropOnPlayerExit")
     end
-    self.logisticBox = self.targets.logisticBox
 
     self.nextDeployTime = 0
     self.lastDriverIsIn = false
@@ -29,7 +31,7 @@ function LogisticSupply:Update()
 
         for i, seat in pairs(self.vehicleObject.seats) do
             if not seat.hasWeapons then
-                hasPassengers = hasPassengers or seat.occupant ~= nil
+                hasPassengers = hasPassengers or (seat.occupant ~= nil and seat.occupant ~= Player.actor)
             end
         end
 
@@ -38,15 +40,20 @@ function LogisticSupply:Update()
         local flag2 = not playerIsInside and self.lastPlayerIsInside and self.dropOnPlayerExit
 
         local flag3 = false
-        if self.weapon ~= nil then
-            local flag3 = self.weapon.isHoldingFire and self.weapon.canFire
-            
-        end
+        -- if self.weapon ~= nil then
+        --     local flag3 = self.weapon.isHoldingFire and self.weapon.canFire
+        -- end
 
         if flag1 or flag2 or flag3 then
-            GameObject.Instantiate(self.logisticBox, self.vehicleObject.transform.position + self.spawnOffset.x * self.vehicleObject.transform.right + self.spawnOffset.y * self.vehicleObject.transform.up + self.spawnOffset.z * self.vehicleObject.transform.forward, self.vehicleObject.transform.rotation)
+            if self.weapon == nil then
+                GameObject.Instantiate(self.logisticBox, self.vehicleObject.transform.position + self.spawnOffset.x * self.vehicleObject.transform.right + self.spawnOffset.y * self.vehicleObject.transform.up + self.spawnOffset.z * self.vehicleObject.transform.forward, self.vehicleObject.transform.rotation)
 
-            self.nextDeployTime = Time.time + self.reloadTime
+                self.nextDeployTime = Time.time + self.reloadTime
+            else
+                if self.weapon.canFire then
+                    self.weapon.Shoot(true)
+                end
+            end
         end
 
         self.lastDriverIsIn = driverIsIn
