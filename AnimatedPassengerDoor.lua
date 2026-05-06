b97d74e707a5b419c8fc6a55ef475465214f285c
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-05-06 11:07:42 +0700

    Update AnimatedPassengerDoor.lua --> v1.0.1
    Add nil checks

--- AnimatedPassengerDoor.lua
+++ AnimatedPassengerDoor.lua
@@ -1,4 +1,4 @@
-behaviour("AnimatedPassengerDoor") --v1.0.0
+behaviour("AnimatedPassengerDoor") --v1.0.1
 
 function AnimatedPassengerDoor:Start()
     self.animator = self.targets.animator.GetComponent(Animator)
@@ -12,10 +12,21 @@ function AnimatedPassengerDoor:Update()
     local isFull = self.vehicle.GetEmptySeat() == nil
     local isLow = self.heightChecker <= 8
     local isSlow = self.vehicle.rigidbody.velocity.sqrMagnitude <= 200
-    local isCloseToLZ = self.vehicle.driver.squad.claimedLandingZone.position - self.vehicle.transform.position
-    isCloseToLZ.y = 0
-    isCloseToLZ = isCloseToLZ.sqrMagnitude <= 15000
-    local isPickingUp = self.vehicle.driver.squad.hasLanded or self.vehicle.driver.squad.isPerformingLanding
+    local isCloseToLZ = false
+    local isPickingUp = false
+    
+    if self.vehicle.driver ~= nil then
+        if self.vehicle.driver.squad ~= nil then
+            if self.vehicle.driver.squad.claimedLandingZone ~= nil then
+                isCloseToLZ = self.vehicle.driver.squad.claimedLandingZone.position - self.vehicle.transform.position
+                isCloseToLZ.y = 0
+                isCloseToLZ = isCloseToLZ.sqrMagnitude <= 15000
+            end
+
+            isPickingUp = self.vehicle.driver.squad.hasLanded or self.vehicle.driver.squad.isPerformingLanding
+        end
+    end
+    
 
     local doorOpen = (isLow and not isFull and isSlow) or (isCloseToLZ) or (isPickingUp)
 
