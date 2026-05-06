5399222d3ca39e115fc07f05f3eca84d6b4a49c4
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-05-06 12:17:53 +0700

    Update CoaxFireButton.lua --> v1.2.1
    Fix audio bug

--- CoaxFireButton.lua
+++ CoaxFireButton.lua
@@ -1,24 +1,28 @@
-behaviour("CoaxFireButton") --v1.2.0
+behaviour("CoaxFireButton") --v1.2.1
 
 function CoaxFireButton:Start()
     self.coax = self.targets.coax.GetComponent(Weapon)
 
     --self.vehicle = self.targets.vehicleObject.GetComponent(Vehicle)
-    self.audio = self.targets.audio.GetComponent(AudioSource)
+    self.audio = self.targets.coax.GetComponent(AudioSource)
     self.seat = self.targets.seatTarget.GetComponent(Seat)
     self.dataContainer = self.gameObject.GetComponent(DataContainer)
     self.keybind = self.dataContainer.GetString("keybind")
 end
 
-function CoaxFireButton:Update()
+function CoaxFireButton:LateUpdate()
     if self.seat.activeWeapon == self.coax then
         if Input.GetKey(self.keybind) then
-            self.coax.Shoot(false)
+            if self.coax.canFire then
+                self.coax.Shoot(true)
+            end
+            self.audio.volume = 1
         end
     else
         if Input.GetKey(self.keybind) and self.seat.occupant == Player.actor then
             if Input.GetKeyDown(self.keybind) and self.coax.canFire then
                 self.audio.Play()
+                self.audio.volume = 1
             end
 
             if self.coax.canFire then
@@ -26,7 +30,7 @@ function CoaxFireButton:Update()
             end
         end
 
-        if Input.GetKeyUp(self.keybind) then
+        if not Input.GetKey(self.keybind) then
             self.audio.Stop()
         end
     end
