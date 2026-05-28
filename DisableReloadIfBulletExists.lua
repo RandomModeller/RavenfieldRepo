1d208429f184fe0c14eb2745b6e878105b2d5189
Debil <71475850+ProfessionalDebil@users.noreply.github.com>
2026-05-28 15:31:30 +0700

    Update DisableReloadIfBulletExists.lua --> v1.0.1
    Switched to change spareAmmo

--- DisableReloadIfBulletExists.lua
+++ DisableReloadIfBulletExists.lua
@@ -1,4 +1,4 @@
-behaviour("DisableReloadIfBulletExists") --v1.0.0
+behaviour("DisableReloadIfBulletExists") --v1.0.1
 
 function DisableReloadIfBulletExists:Start()
     self.weapon = self.gameObject.GetComponent(Weapon)
@@ -10,12 +10,12 @@ end
 
 function DisableReloadIfBulletExists:Update()
     if self.lastProjectile == nil then
-        self.weapon:UnlockWeapon()
+        self.weapon.spareAmmo = 1
     else
         if self.lastProjectile.isActive then
-            self.weapon:LockWeapon()
+            self.weapon.spareAmmo = 0
         else
-            self.weapon:UnlockWeapon()
+            self.weapon.spareAmmo = 1
         end
     end
 end
