099db7f6d800862e89bf4f36dff5129a7165b70b
RandomModeller <71475850+RandomModeller@users.noreply.github.com>
2026-07-02 16:43:34 +0700

    Add UVOffsetRandomizer.lua v1.0.0

---
+++ UVOffsetRandomizer.lua
@@ -0,0 +1,14 @@
+behaviour("UVOffsetRandomizer") --v1.0.0
+
+function UVOffsetRandomizer:Start()
+    self.renderer = self.gameObject.GetComponent(MeshRenderer)
+    self.dataContainer = self.gameObject.GetComponent(DataContainer)
+
+    self.offsetMin = self.dataContainer.GetVector("offsetMin")
+    self.offsetMax = self.dataContainer.GetVector("offsetMax")
+
+    self.renderer.material.mainTextureOffset = Vector2(Random.Range(self.offsetMin.x, self.offsetMax.x), Random.Range(self.offsetMin.y, self.offsetMax.y))
+end
+
+function UVOffsetRandomizer:Update()
+end
